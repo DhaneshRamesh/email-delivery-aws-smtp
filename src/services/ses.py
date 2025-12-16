ea6c996eece4a13b89ffe33eeaabfafb7733cea7
Dhanesh R <dhaneshrameshofficial@gmail.com>
2025-12-16 10:45:05 +0530

    Fix SES region mismatch (use ap-southeast-2 via config default)

--- src/services/ses.py
+++ src/services/ses.py
@@ -30,7 +30,7 @@ class SESService:
     ) -> None:
         self.aws_access_key_id = aws_access_key_id or settings.aws_access_key_id
         self.aws_secret_access_key = aws_secret_access_key or settings.aws_secret_access_key
-        self.region_name = region_name or settings.aws_region_name
+        self.region_name = region_name or settings.aws_region_name or "ap-southeast-2"
         self._client = None
 
     def _client_or_raise(self):
